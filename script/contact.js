3f09ef96beb32603defb40d5cb49f9c49c04ff19
ariskiflan <ariskiflan98@gmail.com>
2023-12-29 17:55:23 +0700

    add contact.js and add form submission in contact page

---
+++ script/contact.js
@@ -0,0 +1,29 @@
+function submitData() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phoneNumber = document.getElementById("number").value;
+  const subject = document.getElementById("select").value;
+  const message = document.getElementById("textarea").value;
+
+  if (
+    name == "" ||
+    email == "" ||
+    phoneNumber == "" ||
+    subject == "" ||
+    message == ""
+  ) {
+    return alert("input field ada yang belum terisi!");
+  }
+
+  console.log(name);
+  console.log(email);
+  console.log(phoneNumber);
+  console.log(subject);
+  console.log(message);
+
+  let a = document.createElement("a");
+  a.href = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(
+    `${message}, nama saya ${name}, kontak saya di ${email}`
+  )}`;
+  a.click();
+}

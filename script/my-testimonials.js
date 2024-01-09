b65d28cb78c3e9475700e602e3f8e52ed02bfb50
ariskiflan <ariskiflan98@gmail.com>
2024-01-10 02:43:42 +0700

    implementation ajax, async await, promise in testimonial page

--- script/my-testimonials.js
+++ script/my-testimonials.js
@@ -51,47 +51,116 @@
 // document.querySelector(".testimonials-box").innerHTML = myTestimonialEl;
 
 ////////////// HOF //////////////
-const testimonials = [
-  {
-    image:
-      "https://awsimages.detik.net.id/community/media/visual/2022/04/01/manga-one-piece_43.webp?w=600&q=90",
-    content: "Makasih",
-    author: "Luffy",
-    rating: 1,
-  },
-  {
-    image:
-      "https://c4.wallpaperflare.com/wallpaper/605/539/788/one-piece-anime-roronoa-zoro-wallpaper-preview.jpg",
-    content: "Good Game!",
-    author: "Zoro",
-    rating: 2,
-  },
-  {
-    image:
-      "https://cdn0-production-images-kly.akamaized.net/VlfIPSwzuGv-jlIDNf6orv3L4WY=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/980310/original/053143900_1441626669-sanji-472600-untitled_35.png",
-    content: "mantab sekali!",
-    author: "Sanji",
-    rating: 2,
-  },
-  {
-    image:
-      "https://cdn0-production-images-kly.akamaized.net/VlfIPSwzuGv-jlIDNf6orv3L4WY=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/980310/original/053143900_1441626669-sanji-472600-untitled_35.png",
-    content: "mantab sekali!",
-    author: "Sanji",
-    rating: 4,
-  },
-  {
-    image:
-      "https://cdn0-production-images-kly.akamaized.net/VlfIPSwzuGv-jlIDNf6orv3L4WY=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/980310/original/053143900_1441626669-sanji-472600-untitled_35.png",
-    content: "mantab sekali!",
-    author: "Sanji",
-    rating: 5,
-  },
-];
+// const testimonials = [
+//   {
+//     image:
+//       "https://awsimages.detik.net.id/community/media/visual/2022/04/01/manga-one-piece_43.webp?w=600&q=90",
+//     content: "Makasih",
+//     author: "Luffy",
+//     rating: 1,
+//   },
+//   {
+//     image:
+//       "https://c4.wallpaperflare.com/wallpaper/605/539/788/one-piece-anime-roronoa-zoro-wallpaper-preview.jpg",
+//     content: "Good Game!",
+//     author: "Zoro",
+//     rating: 2,
+//   },
+//   {
+//     image:
+//       "https://cdn0-production-images-kly.akamaized.net/VlfIPSwzuGv-jlIDNf6orv3L4WY=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/980310/original/053143900_1441626669-sanji-472600-untitled_35.png",
+//     content: "mantab sekali!",
+//     author: "Sanji",
+//     rating: 2,
+//   },
+//   {
+//     image:
+//       "https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/11/28/3045962496.jpg",
+//     content: "boleh juga",
+//     author: "usop",
+//     rating: 4,
+//   },
+//   {
+//     image:
+//       "https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/682/2023/10/28/20231028_140338-4278103081.jpg",
+//     content: "asikkk bang",
+//     author: "nami",
+//     rating: 5,
+//   },
+// ];
 
-const ratingBtnAll = document.querySelector(".rating-btn-el");
+// const ratingBtnAll = document.querySelector(".rating-btn-el");
 
-const allTestimonials = function () {
+// const allTestimonials = function () {
+//   const myTestimonialsEL = testimonials.map((item) => {
+//     return `
+//     <div class="box">
+//     <a href="detail-project.html">
+//       <img src="${item.image}" alt="" class="testimonials-img" />
+//     </a>
+//     <p class="testimonials-project-description">${item.content}</p>
+//     <h3 class="testimonials-project-title">- ${item.author}</h3>
+//     <p class="testimonials-project-rating">
+//       ${item.rating}<i class="ri-star-fill"></i>
+//     </p>
+//   </div>
+//          `;
+//   });
+//   document.querySelector(".testimonials-box").innerHTML =
+//     myTestimonialsEL.join(" ");
+// };
+// allTestimonials();
+// ratingBtnAll.addEventListener("click", allTestimonials);
+
+// function filterTestimonials(rating) {
+//   const filteredTestimonial = testimonials
+//     .filter((item) => item.rating === rating)
+//     .map((item) => {
+//       return `
+//       <div class="box">
+//       <a href="detail-project.html">
+//         <img src="${item.image}" alt="" class="testimonials-img" />
+//       </a>
+//       <p class="testimonials-project-description">${item.content}</p>
+//       <h3 class="testimonials-project-title">- ${item.author}</h3>
+//       <p class="testimonials-project-rating">
+//         ${item.rating}<i class="ri-star-fill"></i>
+//       </p>
+//     </div>
+//          `;
+//     });
+
+//   document.querySelector(".testimonials-box").innerHTML =
+//     filteredTestimonial.join(" ");
+// }
+
+////////////// ASYNC AWAIT, PROMISE, AJAX //////////////
+
+function getDataTestimonials() {
+  return new Promise((myResolve, myReject) => {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "https://api.npoint.io/22de2ebc1f585670095a", true); // mengirim request
+
+    xhttp.onload = () => {
+      if (xhttp.status === 200) {
+        const responseText = JSON.parse(xhttp.responseText); // merubah data yang di ambil dari fake api, string menjadi object
+        myResolve(responseText); // jika dipenuhi
+      } else {
+        myReject("ERROR!"); // jika tidak terpenuhi
+      }
+    };
+
+    xhttp.onerror = () => {
+      myReject("Network Error!");
+    };
+
+    xhttp.send(); // mengirim request
+  });
+}
+
+async function allTestimonials() {
+  const testimonials = await getDataTestimonials();
   const myTestimonialsEL = testimonials.map((item) => {
     return `
     <div class="box">
@@ -104,16 +173,15 @@ const allTestimonials = function () {
       ${item.rating}<i class="ri-star-fill"></i>
     </p>
   </div>
-    
          `;
   });
   document.querySelector(".testimonials-box").innerHTML =
     myTestimonialsEL.join(" ");
-};
+}
 allTestimonials();
-ratingBtnAll.addEventListener("click", allTestimonials);
 
-function filterTestimonials(rating) {
+async function filterTestimonials(rating) {
+  const testimonials = await getDataTestimonials();
   const filteredTestimonial = testimonials
     .filter((item) => item.rating === rating)
     .map((item) => {
@@ -131,6 +199,15 @@ function filterTestimonials(rating) {
          `;
     });
 
+  if (!filteredTestimonial.length) {
+    return (document.querySelector(
+      ".testimonials-box"
+    ).innerHTML = `<div class="not-found-box">
+      <img src="img/notfound.jpg" alt="" class="not-found img" />
+      <h1 class="not-found-title">Data not found!</h1>
+      </div>
+      `);
+  }
   document.querySelector(".testimonials-box").innerHTML =
     filteredTestimonial.join(" ");
 }
